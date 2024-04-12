f80993beee4b8f56e64c979d9788853bdef225fb
Mike Ross <mikewire@gmail.com>
2024-04-11 19:05:51 -0500

    Update ListISOs.ps1

--- ListISOs.ps1
+++ ListISOs.ps1
@@ -17,5 +17,7 @@ Connect-VIServer -Server $server -Credential $creds -Force
 Get-Cluster -PipelineVariable clustername | Get-VMHost | Get-VM | Where-Object {$_.PowerState –eq “PoweredOn”} | Get-CDDrive | 
 Where-Object {$_.IsoPath -ne $null} | Select-Object -Property @{N='vCenter'; E={([uri]($_.Parent.ExtensionData.Client.ServiceUrl)).Host}}, @{N=’Cluster’;E={$clustername.Name}}, @{N='VM'; E={$_.Parent.Name }}, @{N='ISO'; E={$_.IsoPath}} | 
 Format-Table
+# Disconnect from VIServer
+Disconnect-VIServer -Server <yourserver>
 # End
 #######################################################
