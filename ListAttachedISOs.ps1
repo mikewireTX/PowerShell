09d515227c8f8d4d5306486a90c61af4abf8b27b
Mike Ross <mikewire@gmail.com>
2024-04-24 14:27:40 -0500

    Create ListAttachedISOs.ps1

---
+++ ListAttachedISOs.ps1
@@ -0,0 +1,23 @@
+﻿#######################################################
+#       Get Iso's attached to VM's in vCenter Report
+#######################################################
+# Author(s):  Mike Ross  
+# Github:  https://github.com/mikewireTX
+# Web: https://mikewire.com/
+#######################################################
+#
+# Connect to vCenter
+$creds = Get-Credential
+# Put your vCenter name here
+$server = Read-Host 'Input the vCenter server FQDN or IP'
+Connect-VIServer -Server $server -Credential $creds -Force
+#########################
+#########################
+# Get a list of VM's with ISO's attached and list vCenter, Cluster, VM name and ISO
+Get-Cluster -PipelineVariable clustername | Get-VMHost | Get-VM | Where-Object {$_.PowerState –eq “PoweredOn”} | Get-CDDrive | 
+Where-Object {$_.IsoPath -ne $null} | Select-Object -Property @{N='vCenter'; E={([uri]($_.Parent.ExtensionData.Client.ServiceUrl)).Host}}, @{N=’Cluster’;E={$clustername.Name}}, @{N='VM'; E={$_.Parent.Name }}, @{N='ISO'; E={$_.IsoPath}} | 
+Format-Table
+# Disconnect from VIServer
+Disconnect-VIServer -Server $server
+# End
+#######################################################
